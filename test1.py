caadd878404d6d6dd0e5a6eedd404fe733580e2f
jbl1234 <lijibo@inspur.com>
2019-03-02 15:09:00 +0800

    Delete a line

--- test1.py
+++ test1.py
@@ -12,7 +12,7 @@ x = tf.placeholder(tf.float32)
 linear_model = W*x + b
 init = tf.global_variables_initializer()
 sess.run(init)
-#print(sess.run(linear_model, {x:[1,2,3,4]}))
+
 y = tf.placeholder(tf.float32)
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
